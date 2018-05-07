093bfa9ef4247c8c36435e075018566593d3aaca
Bentley James Oakes <bentleyjoakes@gmail.com>
2018-05-07 13:17:31 -0400

    Update run local script to shut down server and mt when exiting.

--- run_AToMPM_local.sh
+++ run_AToMPM_local.sh
@@ -4,5 +4,15 @@
 set -e
 
 node httpwsd.js &
+serverpid=$!
 sleep 3
-python mt/main.py
+
+python2 mt/main.py&
+mtpid=$!
+sleep 1
+
+chromium --app=http://localhost:8124/atompm
+
+kill $serverpid
+kill $mtpid
+echo "All done!"
