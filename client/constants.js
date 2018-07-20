28c4494f3e006e3e88cdd88cd05bdbe26cd8d5a9
Bentley James Oakes <bentleyjoakes@gmail.com>
2018-07-20 16:37:33 -0400

    Change version number for release v0.8.1

--- client/constants.js
+++ client/constants.js
@@ -7,7 +7,7 @@
 var __WEBPAGE__ = 'https://atompm.github.io/',
 	__RELEASE_LOC__ = "https://api.github.com/repos/AToMPM/atompm/releases/latest",
 	__DOC_WEBPAGE__ = "https://atompm.readthedocs.io",
-    __VERSION__ = '0.8.0',
+    __VERSION__ = '0.8.1',
     __DEFAULT_SAVEAS		 	= '.autosave.model',
 	 __TITLE						= 'AToMPM',
 	 __EXITWARNING				= 'There are unsaved changes. Proceeding will cause'+
