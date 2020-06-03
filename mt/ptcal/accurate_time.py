75ed189752ac6802c4ef22987f260852dab8ef78
bentleyjoakes <bentleyjoakes@gmail.com>
2020-06-03 10:51:22 +0200

    Remove deprecated time.clock().

--- mt/ptcal/accurate_time.py
+++ mt/ptcal/accurate_time.py
@@ -3,19 +3,12 @@ Copyright 2011 by the AToMPM team and licensed under the LGPL
 See COPYING.lesser and README.md in the root of this project for full details'''
 
 import time as t
-import os
 
 global start_time
 def set_start_time():
     global start_time
-    if os.name == 'posix':
-        start_time = t.time()
-    elif os.name == 'nt':
-        start_time = t.clock()
+    start_time = t.time()
 
-if os.name == 'posix':
-    def time():
+def time():
         return int((t.time() - start_time) * 1000)
-elif os.name == 'nt':
-    def time():
-        return int((t.clock() - start_time) * 1000)
+
