23a411ca7b8d99e7b51b15756f147feb2a00be65
Bentley James Oakes <bentley.oakes@uantwerpen.be>
2020-06-09 20:45:32 +0200

    Update version to 0.8.5.

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'The AToMPM Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.8.4'
+release = '0.8.5'
 
 
 # -- General configuration ---------------------------------------------------
