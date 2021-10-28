4da63ccb910193977c6021353b6b185b5b072d0f
bentleyjoakes <bentleyjoakes@gmail.com>
2021-10-28 16:18:26 -0400

    Fix Windows Python bat.

--- packaging/windows_scripts/Python.bat
+++ packaging/windows_scripts/Python.bat
@@ -2,10 +2,6 @@
 TITLE AToMPM
 CD ".\atompm"
 
-IF EXIST "..\platform\WinPython" (
-"..\platform\WinPython\python-3.6.5\python.exe" mt\main.py
-) ELSE (
-"..\platform\PortablePython27\Python-Portable.exe" mt\main.py
-)
+"..\platform\WinPython\python-3.9.5.amd64\python.exe" mt\main.py
 
 
