12fcce8dbcebeff24ba9982667c56f9dc8c99eac
vasco-sousa <38866010+vasco-sousa@users.noreply.github.com>
2022-05-11 10:58:41 -0400

    Nightwatch (#135)
    Updated configuration of nightwatch configuration for continuous integration.
    Dropped the use of xvfb as that seemed to be the source of incompatibility
    it runs properly if it is headless if it has a window size specified, otherwise it also fails as it did with xvfb
    Added the individual tests to the workflow for granularity
    Added command parameters to test script for convenience
    using the keyword headless allows to run the tests in headless mode
    passing a specific test file as the last argument for the convenience of not having to launch the servers by hand

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -19,7 +19,25 @@ module.exports = {
         default: {
             launch_url: 'http://localhost:8124/atompm',
             desiredCapabilities: {
-                browserName: 'chrome'
+                browserName: 'chrome',
+                'goog:chromeOptions': {
+                    'args':[
+                        'disable-gpu','remote-debugging-port=9222'
+                    ]
+                }
+            }
+        },
+        run_headless: {
+            launch_url: 'http://localhost:8124/atompm',
+            desiredCapabilities: {
+                browserName: 'chrome',
+                'goog:chromeOptions': {
+                    'args':[
+                        'headless',
+                        'window-size=2880,1800',
+                        'disable-gpu','remote-debugging-port=9222'
+                    ]
+                }
             }
         }
     },
