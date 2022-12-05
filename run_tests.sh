6bb86256fa99458d1f2da3864b4db817e4fab611
vasco-sousa <dasilvav@iro.umontreal.ca>
2022-12-05 11:58:34 -0500

    outputs per test

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,22 @@
 #exit on errors
 set -e
 
+if [ "$#" -ge 1 ] 
+then
+    if [ "$#" -ge 2 ] 
+    then
+        logname="${2:8:-3}_"
+    else
+        logname="${1:8:-3}_"
+    fi
+else
+logname=""
+fi
+
+
 #run server
 echo "Starting server..."
-node httpwsd.js > ./logs/node.log 2> ./logs/node_error.log &
+node httpwsd.js > "./logs/${logname}node.log" 2> "./logs/${logname}node_error.log" &
 serverpid=$!
 sleep 3
 
@@ -18,7 +31,7 @@ fi
 
 #run mt script
 echo "Starting model transformation script..."
-python3 mt/main.py > ./logs/python.log 2> ./logs/python_error.log &
+python3 mt/main.py > "./logs/${logname}python.log" 2> "./logs/${logname}python_error.log" &
 mtpid=$!
 sleep 3
 
